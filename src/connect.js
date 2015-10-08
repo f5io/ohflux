5c17e27a15b736faa43acaffa6e395ead539fd78
Joe Harlow <joe@f5.io>
2015-10-08 14:19:01 +0100

    cache the modified stream in `connect`

--- src/connect.js
+++ src/connect.js
@@ -3,11 +3,11 @@ export default function connect(Store, modifier = x => x) {
     getInitialState() {
       let state;
       let fn = s => state = s;
-      modifier(Store).onValue(fn).offValue(fn);
+      this._storeStream = modifier(Store);
+      this._storeStream.onValue(fn).offValue(fn);
       return state;
     },
     componentDidMount() {
-      this._storeStream = modifier(Store);
       this._storeStreamHandler = state => this.setState(state);
       this._storeStream.onValue(this._storeStreamHandler);
     },
